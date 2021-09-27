d49a295bc6508ffd942ae1b1f85c274f7be31f70
FunUndead <shix97@mail.ru>
2021-09-27 19:16:24 +0400

    diplom3

---
+++ settings.py
@@ -0,0 +1,4 @@
+token = '' # Токен группы
+tokenapp = '' # Токен приложения
+DSN = 'postgresql+psycopg2://user:pass@localhost:5432/db_name' # Данные бд
+DSNTEST = 'postgresql+psycopg2://user:pass@localhost:5432/db_name' #тестовое соединение
